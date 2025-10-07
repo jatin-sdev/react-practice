863fb0063ce3ad4ea9bd8dc4f88ac6137120bf10
jatin-sdev <jatin.sdev@gmail.com>
2025-10-07 23:04:57 +0530

    added feature of reset and inc by amount

--- redux-play/src/features/counter/counter.jsx
+++ redux-play/src/features/counter/counter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment } from "./counterSlice.jsx";
+import { decrement, increment, reset, incrementByAmount } from "./counterSlice.jsx";
 
 
 
@@ -8,10 +8,11 @@ import { decrement, increment } from "./counterSlice.jsx";
 export function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  const [amount, setAmount] = React.useState(0);
 
   return (
     <div>
-      <div style={{ display: "flex", gap: "2rem", alignItems: "center" }}>
+      <div style={{ display: "flex", gap: "1rem", alignItems: "center" }}>
         <button
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
@@ -25,7 +26,21 @@ export function Counter() {
         >
           -
         </button>
+        <button onClick={() => dispatch(reset())}>
+          reset
+        </button>
+        <div><input 
+          type="number"
+          aria-label="Set increment amount"
+        placeholder="enter amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        />
+        <button onClick={() => dispatch(incrementByAmount(Number(amount)))}>
+          Add Amount
+        </button>
       </div>
+    </div>
     </div>
   );
 }
